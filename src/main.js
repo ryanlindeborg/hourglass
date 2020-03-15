77ef8c35af8abc4d5dbafe0991e1064b079db990
Ryan Lindeborg <lindeborgryan@gmail.com>
2020-03-15 17:09:03 -0400

    Add global css file

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+// Import global css file
+import '@/css/main.css';
+
 Vue.config.productionTip = false;
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <section class="splash">test text</section>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
