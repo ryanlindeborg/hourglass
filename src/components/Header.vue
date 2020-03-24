90a3f5e51ba295f8e57f295de0f98f120024ed2e
ryanlindeborg <lindeborgryan@gmail.com>
2020-03-23 20:52:24 -0700

    Update header and add Bootstrap library

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,9 +2,17 @@
   <div class="header">
     <nav>
       <router-link to="/"><img src="/images/timeline-logo.png" alt="Timeline" /></router-link>
-      <p><router-link to="/my-career">My Career</router-link></p>
-      <p><router-link to="/search">Search</router-link></p>
-      <p><router-link to="/profile-library">Profile Library</router-link></p>
+      <section>
+        <p onclick="window.location = '/my-career'">
+          <router-link to="/my-career">My Career</router-link>
+        </p>
+        <p onclick="window.location = '/search'">
+          <router-link to="/search">Search</router-link>
+        </p>
+        <p onclick="window.location = '/profile-library'">
+          <router-link to="/profile-library">Profile Library</router-link>
+        </p>
+      </section>
     </nav>
   </div>
 </template>
@@ -14,5 +22,8 @@
 <style scoped>
   nav { background-color: #b5b7bd; height: 4em; }
   img { display: inline-block; margin: 0.3em; height: 80%; }
-  p { display: inline-block; }
+  section { float: right; margin-right: 1em; }
+  p { display: inline-block; padding: 1.2em; margin: 0; }
+  p:hover { cursor: pointer; background-color: #abacb3; }
+  a:hover { text-decoration: none; size: 1.5em; }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="home">
-    <section class="splash">test text</section>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="splash">
+      <h1>Seize your next career opportunity, and learn from those who have done it before you</h1>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld.vue';
+// import HelloWorld from '../components/HelloWorld.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
 };
 </script>
+
+<style scoped>
+  section.splash { background-image: linear-gradient(to bottom right, #87cafa, #1167b1);
+    padding: 2em 0; height: 50vh; display: flex; flex-direction: column; justify-content: center; }
+  h1 { color: white; text-align: center; font-weight: 600; font-style: italic; width: 70%;
+    margin: auto; }
+</style>
