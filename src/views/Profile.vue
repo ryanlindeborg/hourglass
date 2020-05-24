088453c5b0c3aec01f5f0d40a50e921631d49802
ryanlindeborg <lindeborgryan@gmail.com>
2020-05-23 21:03:57 -0700

    Add to Profile View

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -5,6 +5,39 @@
     <h2>{{ profileDetails.currentJob.position }} at
       {{ profileDetails.currentJob.company.name }}</h2>
     <h3>College: {{ profileDetails.collegeSchoolUser.school.name }}</h3>
+    <section>
+      <h4>Current Job</h4>
+      <p>Company: {{ profileDetails.currentJob.company.name }}</p>
+      <p>Position: {{ profileDetails.currentJob.position }}</p>
+      <p>Industry: {{ profileDetails.currentJob.company.industry }}</p>
+    </section>
+    <section>
+      <h4>Current Job</h4>
+      <p>Company: {{ profileDetails.currentJob.company.name }}</p>
+      <p>Position: {{ profileDetails.currentJob.position }}</p>
+      <p>Industry: {{ profileDetails.currentJob.company.industry }}</p>
+    </section>
+    <section class="education-history">
+      <h4>College</h4>
+      <section class="college">
+        <p>School: {{ profileDetails.collegeSchoolUser.school.name }}</p>
+        <p>Degree: {{ profileDetails.collegeSchoolUser.degree }}</p>
+        <p>Field of Study: {{ profileDetails.collegeSchoolUser.fieldOfStudy }}</p>
+      </section>
+      <h4>Postgraduate Education</h4>
+      <section class="postgrad">
+        <p>School: {{ profileDetails.postGradSchoolUser.school.name }}</p>
+        <p>Degree: {{ profileDetails.postGradSchoolUser.degree }}</p>
+        <p>Field of Study: {{ profileDetails.postGradSchoolUser.fieldOfStudy }}</p>
+      </section>
+    </section>
+    <section class="dream">
+      <h4>Me in 10 Years</h4>
+      <h5>Dream Job</h5>
+      <p>Company: {{ profileDetails.dreamJob.company.name }}</p>
+      <p>Position: {{ profileDetails.dreamJob.position }}</p>
+      <p>Industry: {{ profileDetails.dreamJob.company.industry }}</p>
+    </section>
   </div>
 </template>
 
@@ -15,7 +48,7 @@ export default {
   data() {
     return {
       profileDetails: '',
-
+      // TODO: Loading wheel paradigm where take away loading GIF once have set the data
     };
   },
   props: ['userId'],
