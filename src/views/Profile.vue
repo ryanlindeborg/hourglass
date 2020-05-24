ac558b1848c7ac31eb13f97bf5d6ecefdd963701
ryanlindeborg <lindeborgryan@gmail.com>
2020-05-24 12:19:52 -0700

    Add to profile view and include FontAwesome library

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,42 +1,43 @@
 <template>
   <div>
-    <h1>Hourglass Profile: {{ profileDetails.user.firstName }}
-      {{ profileDetails.user.lastName }}</h1>
-    <h2>{{ profileDetails.currentJob.position }} at
-      {{ profileDetails.currentJob.company.name }}</h2>
-    <h3>College: {{ profileDetails.collegeSchoolUser.school.name }}</h3>
-    <section>
-      <h4>Current Job</h4>
-      <p>Company: {{ profileDetails.currentJob.company.name }}</p>
-      <p>Position: {{ profileDetails.currentJob.position }}</p>
-      <p>Industry: {{ profileDetails.currentJob.company.industry }}</p>
+    <p></p>
+    <section class="splash">
+      <img src="/images/RL-headshot-square.png" :alt="userFullName" />
+      <h1>Hourglass Profile: {{ userFullName }}</h1>
+      <h2>{{ profileDetails.currentJob.position }} at
+        {{ profileDetails.currentJob.company.name }}</h2>
     </section>
-    <section>
-      <h4>Current Job</h4>
-      <p>Company: {{ profileDetails.currentJob.company.name }}</p>
-      <p>Position: {{ profileDetails.currentJob.position }}</p>
-      <p>Industry: {{ profileDetails.currentJob.company.industry }}</p>
-    </section>
-    <section class="education-history">
-      <h4>College</h4>
-      <section class="college">
-        <p>School: {{ profileDetails.collegeSchoolUser.school.name }}</p>
-        <p>Degree: {{ profileDetails.collegeSchoolUser.degree }}</p>
-        <p>Field of Study: {{ profileDetails.collegeSchoolUser.fieldOfStudy }}</p>
+    <section class="profile-details">
+      <section>
+        <h3>Current Job</h3>
+        <p>Company: {{ profileDetails.currentJob.company.name }}</p>
+        <p>Position: {{ profileDetails.currentJob.position }}</p>
+        <p>Industry: {{ profileDetails.currentJob.company.industry }}</p>
       </section>
-      <h4>Postgraduate Education</h4>
-      <section class="postgrad">
-        <p>School: {{ profileDetails.postGradSchoolUser.school.name }}</p>
-        <p>Degree: {{ profileDetails.postGradSchoolUser.degree }}</p>
-        <p>Field of Study: {{ profileDetails.postGradSchoolUser.fieldOfStudy }}</p>
+      <section class="education-history">
+        <i class="fas fa-graduation-cap"></i>
+        <div>
+          <h3>College</h3>
+          <section>
+            <p>School: {{ profileDetails.collegeSchoolUser.school.name }}</p>
+            <p>Degree: {{ profileDetails.collegeSchoolUser.degree }}</p>
+            <p>Field of Study: {{ profileDetails.collegeSchoolUser.fieldOfStudy }}</p>
+          </section>
+          <h3>Postgraduate Education</h3>
+          <section>
+            <p>School: {{ profileDetails.postGradSchoolUser.school.name }}</p>
+            <p>Degree: {{ profileDetails.postGradSchoolUser.degree }}</p>
+            <p>Field of Study: {{ profileDetails.postGradSchoolUser.fieldOfStudy }}</p>
+          </section>
+        </div>
       </section>
-    </section>
-    <section class="dream">
-      <h4>Me in 10 Years</h4>
-      <h5>Dream Job</h5>
-      <p>Company: {{ profileDetails.dreamJob.company.name }}</p>
-      <p>Position: {{ profileDetails.dreamJob.position }}</p>
-      <p>Industry: {{ profileDetails.dreamJob.company.industry }}</p>
+      <section class="dream">
+        <h3>Me in 10 Years</h3>
+        <h4>Dream Job</h4>
+        <p>Company: {{ profileDetails.dreamJob.company.name }}</p>
+        <p>Position: {{ profileDetails.dreamJob.position }}</p>
+        <p>Industry: {{ profileDetails.dreamJob.company.industry }}</p>
+      </section>
     </section>
   </div>
 </template>
@@ -48,8 +49,14 @@ export default {
   data() {
     return {
       profileDetails: '',
-      // TODO: Loading wheel paradigm where take away loading GIF once have set the data
+      hasProfileDetailsLoaded: false,
     };
+  },
+  computed: {
+    userFullName() {
+      return (this.profileDetails.user == null ? null
+        : `${this.profileDetails.user.firstName} ${this.profileDetails.user.lastName}`);
+    },
   },
   props: ['userId'],
   created() {
@@ -64,4 +71,11 @@ export default {
 </script>
 
 <style scoped>
+  section.splash { height: 20em; padding-top: 1em; margin: 4vw; }
+  section.splash img { border: 3px solid black; width: 20em; height: 20em; border-radius: 100%;
+    float: right; margin: -1em 8% 0 0; }
+  h1 { font-size: 3.5em; margin-bottom: 2%; }
+  section.profile-details { background-color: #F5F9FA; }
+  section.education-history div { float: right; }
+  i { color: #4390C5; font-size: 3em; }
 </style>
