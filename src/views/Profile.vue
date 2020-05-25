83c52ee7372af335d8ac160aad13911494adad20
ryanlindeborg <lindeborgryan@gmail.com>
2020-05-25 12:20:02 -0700

    RTAS-1 Add conditional formatting and edit button

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -4,22 +4,32 @@
     <section class="splash">
       <img src="/images/RL-headshot-square.png" :alt="userFullName" />
       <h1>Hourglass Profile: {{ userFullName }}</h1>
-      <h2>{{ profileDetails.currentJob.position }} at
-        {{ profileDetails.currentJob.company.name }}</h2>
+      <div class="edit-container">
+        <h2 v-if="profileDetails.currentJob">{{ profileDetails.currentJob.position }} at
+          {{ profileDetails.currentJob.company.name }}</h2>
+        <i class="fas fa-pen edit" v-if="isEditable"></i>
+      </div>
     </section>
     <section class="profile-details">
+      <p v-if="!profileDetails">Looks like we don't have profile details on this user!</p>
       <section class="work-history">
         <i class="fas fa-briefcase"></i>
         <div>
-          <section>
-            <h3>Current Job</h3>
+          <section v-if="profileDetails.currentJob">
+            <div class="edit-container">
+              <h3>Current Job</h3>
+              <i class="fas fa-pen edit" v-if="isEditable"></i>
+            </div>
             <p>Company: {{ profileDetails.currentJob.company.name }}</p>
             <p>Position: {{ profileDetails.currentJob.position }}</p>
             <p>Industry: {{ profileDetails.currentJob.company.industry | parseIndustry }}</p>
           </section>
           <br/>
-          <section>
-            <h3>First Post-College Job</h3>
+          <section v-if="profileDetails.firstPostCollegeJob">
+            <div class="edit-container">
+              <h3>First Post-College Job</h3>
+              <i class="fas fa-pen edit" v-if="isEditable"></i>
+            </div>
             <p>Company: {{ profileDetails.firstPostCollegeJob.company.name }}</p>
             <p>Position: {{ profileDetails.firstPostCollegeJob.position }}</p>
             <p>Industry:
@@ -30,15 +40,21 @@
       <section class="education-history">
         <i class="fas fa-graduation-cap"></i>
         <div>
-          <h3>College</h3>
-          <section>
+          <section v-if="profileDetails.collegeSchoolUser">
+            <div class="edit-container">
+              <h3>College</h3>
+              <i class="fas fa-pen edit" v-if="isEditable"></i>
+            </div>
             <p>School: {{ profileDetails.collegeSchoolUser.school.name }}</p>
             <p>Degree: {{ profileDetails.collegeSchoolUser.degree }}</p>
             <p>Field of Study: {{ profileDetails.collegeSchoolUser.fieldOfStudy }}</p>
           </section>
           <br/>
-          <h3>Postgraduate Education</h3>
-          <section>
+          <section v-if="profileDetails.postGradSchoolUser">
+            <div class="edit-container">
+              <h3>Postgraduate Education</h3>
+              <i class="fas fa-pen edit" v-if="isEditable"></i>
+            </div>
             <p>School: {{ profileDetails.postGradSchoolUser.school.name }}</p>
             <p>Degree: {{ profileDetails.postGradSchoolUser.degree }}</p>
             <p>Field of Study: {{ profileDetails.postGradSchoolUser.fieldOfStudy }}</p>
@@ -48,10 +64,15 @@
       <section class="dream">
         <i class="fas fa-hourglass"></i>
         <div>
-          <h3>Dream Job</h3>
-          <p>Company: {{ profileDetails.dreamJob.company.name }}</p>
-          <p>Position: {{ profileDetails.dreamJob.position }}</p>
-          <p>Industry: {{ profileDetails.dreamJob.company.industry | parseIndustry }}</p>
+          <section v-if="profileDetails.dreamJob">
+            <div class="edit-container">
+              <h3>Dream Job</h3>
+              <i class="fas fa-pen edit" v-if="isEditable"></i>
+            </div>
+            <p>Company: {{ profileDetails.dreamJob.company.name }}</p>
+            <p>Position: {{ profileDetails.dreamJob.position }}</p>
+            <p>Industry: {{ profileDetails.dreamJob.company.industry | parseIndustry }}</p>
+          </section>
         </div>
       </section>
     </section>
@@ -66,6 +87,7 @@ export default {
     return {
       profileDetails: '',
       hasProfileDetailsLoaded: false,
+      isEditable: false,
     };
   },
   computed: {
@@ -106,5 +128,12 @@ export default {
   section.profile-details { background-color: #F5F9FA; padding: 2em 0; }
   section.education-history, section.work-history, section.dream { margin: 4vw; }
   section.education-history div, section.work-history div, section.dream div { padding-left: 40%; }
+  section.education-history section:hover i.edit, section.work-history section:hover i.edit,
+  section.dream section:hover i.edit { visibility: visible; }
+  section.splash div.edit-container:hover i.edit { visibility: visible; }
+  h2, h3 { display: inline-block; }
   i { color: #4390C5; font-size: 8em; padding-left: 4vw; float: left; }
+  div.edit-container { display: inline-block; position: relative; }
+  i.edit { font-size: 1.2em; position: absolute; top: 6px; right: -35px; visibility: hidden; }
+  i.edit:hover { cursor: pointer; }
 </style>
